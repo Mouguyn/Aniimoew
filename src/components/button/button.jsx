f1f07abd9539446bce1dde6af2b8fe22e514b9dd
nera <90453855+Tieubao1@users.noreply.github.com>
2022-05-18 23:45:05 +0700

    Update button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -4,10 +4,10 @@ function Buttons() {
   return (
     <div className="container button-container">
       <a href="#about" className="btn pri">
-        Learn more
+        
       </a>
       <a href="#contact" className="btn sec">
-        Get in Touch
+        
       </a>
     </div>  
   );
