222879a20654c2ff9d28de713db8d373eef5c7a0
nera <90453855+Tieubao1@users.noreply.github.com>
2022-05-19 08:20:11 +0700

    Update nav.jsx

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,7 +1,7 @@
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineUser } from "react-icons/ai";
 import { TiGroupOutline } from "react-icons/ti";
+import { FaGoogleDrive } from "react-icons/fa";
 import { BiMessageRoundedDots } from "react-icons/bi";
 import { BsArrowDownCircle } from "react-icons/bs";
 
@@ -12,13 +12,13 @@ function Navbar() {
         <AiOutlineHome className="icon active-nav" />
       </a>
       <a href="#about">
-        <AiOutlineUser className="icon" />
-      </a>
-      <a href="#members">
         <TiGroupOutline className="icon" />
       </a>
       <a href="#contact">
-        <BiMessageRoundedDots className="icon" />
+      <BiMessageRoundedDots className="icon" />
+      </a>
+      <a href="https://drive.google.com/drive/folders/1--vZ63VuLXmbGR98VL7yTeQYzFwtP9xe?fbclid=IwAR2OuFOVXja1AgKw9-djsTQSAP6TvWmGKYQP6Jdl0DF7JLNtaEM9m0sUkfY">
+      <FaGoogleDrive className="icon" />
       </a>
       <a href="#footer">
         <BsArrowDownCircle className="icon" />
