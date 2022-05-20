a4fae3cd745a17690bf50337b724e1c165a4c782
nera <90453855+Tieubao1@users.noreply.github.com>
2022-05-20 07:31:40 +0700

    Update contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -30,7 +30,7 @@ function Contact() {
           </h2>
         </a>
 
-        <a href="https://www.instagram.com/tieubao_1602/" className="contact instagram">
+        <a href="https://www.instagram.com/mou.guyn_62//" className="contact instagram">
           <AiOutlineInstagram className="icon" />
           <h2>
             instagram <span>Don't tell me for precius</span>
